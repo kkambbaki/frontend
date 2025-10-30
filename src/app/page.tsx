7363c7fe5b06afd68154d852bbac9eae80cf93c9
xuuwon <dumpling1011@gmail.com>
2025-10-30 20:57:21 +0900

    feat: 시작 페이지 반응형 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Input } from '@/components/common/input';
 import Image from 'next/image';
 
 export default function Home() {
@@ -6,18 +5,17 @@ export default function Home() {
     <main className="h-screen flex flex-col">
       <header className="h-10 w-full bg-background-header" />
 
-      {/* 콘텐츠 영역 */}
       <div className="flex flex-col items-center justify-center flex-1 gap-32 font-malrang">
         <div className="flex flex-col items-center">
-          <div className="flex items-center gap-6">
-            <p className="text-[140px]">깜빡이</p>
+          <div className="flex flex-col-reverse items-center md:gap-6 md:flex-row md:justify-center">
+            <p className="text-[100px] md:text-[140px]">깜빡이</p>
             <Image src="/character.png" alt="character" width={100} height={100} />
           </div>
 
-          <p className="text-[32px]">보호자와 함께 플레이하세요!</p>
+          <p className="text-[32px] max-md:text-[25px]">보호자와 함께 플레이하세요!</p>
         </div>
 
-        {/* Button 자리 */}
+        {/* TODO: Button 컴포넌트 추가 */}
         <p className="w-[280px] h-[100px] bg-amber-500 text-[46px] flex justify-center items-center">
           시작하기
         </p>
